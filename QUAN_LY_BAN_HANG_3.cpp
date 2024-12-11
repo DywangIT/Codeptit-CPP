c9318c993b8a0b308124c741a9e61c01b8055327
Quang <dyquang5083@gmail.com>
2024-12-11 23:16:43 +0700

    change file

--- QUAN_LY_BAN_HANG_3.cpp
+++ QUAN_LY_BAN_HANG_3.cpp
@@ -1,91 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define FASTER() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-// Dywang //
+#define faster()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+const ll N = 10000000;
 
-class KH {
-    public: 
-    int code;
-    string name, gender, birth, address;
-};
+int cntkh = 1, cntmh = 1, cnthd = 1;
+map<string, string> KH, MH;
+map<string, int> profit;
 
-class MH {
-    public:
-    int code;
-    string name, unit;
-    ll pur, sell;
-};
-
-map<int, KH> mkh;
-map<int, MH> mmh;
-
-string strip(string s)
+class KhachHang
 {
-    stringstream ss(s);
-    string tok, result = "";
-    while (ss >> tok)
+public:
+    string name, gender, dob, add;
+    friend istream &operator>>(istream &in, KhachHang &x)
     {
-        result += tok;
-        result += " ";
-    }
-    return result.erase(result.length() - 1);
-}
+        getline(in, x.name);
+        getline(in, x.gender);
+        getline(in, x.dob);
+        getline(in, x.add);
 
-void get_KH(){
-    ifstream in("KH.in");
-    int n; in >> n;
-    for(int i = 1; i <= n; i++) {
-        int code = i;
-        string name, gender, birth, address;
-        getline(in >> ws, name);
-        getline(in >> ws, gender);
-        getline(in >> ws, birth);
-        getline(in >> ws, address);
-        mkh[i] = {code, strip(name), strip(gender), strip(birth), strip(address)};
-    }
-}
+        string id = "KH";
+        if (cntkh < 10)
+            id += "00" + to_string(cntkh);
+        else if (cntkh < 100)
+            id += "0" + to_string(cntkh);
+        else
+            id += to_string(cntkh);
+        ++cntkh;
 
-void get_MH() {
-    ifstream in("MH.in");
-    int n; in >> n;
-    for(int i = 1; i <= n; i++){
-        int code = i;
-        string name, unit;
-        ll pur, sell;
-        getline(in >> ws, name);
-        getline(in >> ws, unit);
-        in >> pur >> sell;
-        mmh[i] = {code, strip(name), strip(unit), pur, sell};
+        KH[id] = x.name + " " + x.add;
+        return in;
     }
-}
+};
 
-void show_HD() {
-    ifstream in("HD.in");
-    int n; in >> n;
-    for(int i = 1; i <= n; i++){
-        string code_kh, code_mh;
-        int quanity;
-        in >> code_kh >> code_mh >> quanity;
-        int sttkh = stoi(&code_kh[2]);
-        int sttmh = stoi(&code_mh[2]);
-        cout << "HD" << setfill('0') << setw(3) << i << " ";
-        cout << mkh[sttkh].name << " ";
-        cout << mkh[sttkh].address << " ";
-        cout << mmh[sttmh].name << " ";
-        cout << mmh[sttmh].unit << " ";
-        cout << mmh[sttmh].pur << " ";
-        cout << mmh[sttmh].sell << " ";
-        cout << quanity << " ";
-        cout << mmh[sttmh].sell * quanity << endl;
+class MatHang
+{
+public:
+    string name, unit;
+    int purchase, sell;
+    friend istream &operator>>(istream &in, MatHang &x)
+    {
+        in.ignore();
+        getline(in, x.name);
+        getline(in, x.unit);
+        in >> x.purchase >> x.sell;
+
+        string id = "MH";
+        if (cntmh < 10)
+            id += "00" + to_string(cntmh);
+        else if (cntmh < 100)
+            id += "0" + to_string(cntmh);
+        else
+            id += to_string(cntmh);
+        ++cntmh;
+
+        MH[id] = x.name + " " + x.unit + " " + to_string(x.purchase) + " " + to_string(x.sell);
+        profit[id] = x.sell;
+        return in;
     }
-}
+};
+
+class HoaDon
+{
+public:
+    string kh, mh, id;
+    int amount;
+    friend istream &operator>>(istream &in, HoaDon &x)
+    {
+        in >> x.kh >> x.mh >> x.amount;
+
+        x.id = "HD";
+        if (cnthd < 10)
+            x.id += "00" + to_string(cnthd);
+        else if (cnthd < 100)
+            x.id += "0" + to_string(cnthd);
+        else
+            x.id += to_string(cnthd);
+        ++cnthd;
+
+        return in;
+    }
+    friend ostream &operator<<(ostream &out, HoaDon &x)
+    {
+        out << x.id << " " << KH[x.kh] << " " << MH[x.mh] << " " << x.amount << " " << profit[x.mh] * x.amount << "\n";
+        return out;
+    }
+};
 
 int main()
 {
-    FASTER();
-    get_KH();
-    get_MH();
-    show_HD();
+    ifstream inkh("KH.in");
+    ifstream inmh("MH.in");
+    ifstream inhd("HD.in");
+
+    KhachHang dskh[25];
+    MatHang dsmh[45];
+    HoaDon dshd[105];
+    int N, M, K, i;
+
+    inkh >> N;
+    inkh.ignore();
+    for (i = 0; i < N; i++)
+        inkh >> dskh[i];
+    inmh >> M;
+    for (i = 0; i < M; i++)
+        inmh >> dsmh[i];
+    inhd >> K;
+    for (i = 0; i < K; i++)
+        inhd >> dshd[i];
+
+    for (i = 0; i < K; i++)
+        cout << dshd[i];
     return 0;
 }
